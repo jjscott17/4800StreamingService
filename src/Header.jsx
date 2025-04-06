3b10fe76f54ff3c1fabc09a5cb71827df89691b3
Joseph Scott <165531069+jjscott17@users.noreply.github.com>
2025-04-05 20:44:04 -0700

    Add files via upload

---
+++ src/Header.jsx
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+import './Header.css';
+
+function Header({ onSearchTermChange }) {
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const handleSearchChange = (event) => {
+    setSearchTerm(event.target.value);
+    onSearchTermChange(event.target.value); // Pass search term back to App
+  };
+
+  const handleSearchSubmit = (event) => {
+    event.preventDefault();
+    console.log('Searching for:', searchTerm);
+  };
+
+  return (
+    <div className='Header'>
+      <header>
+        <h1>cosmic</h1>
+        <nav>
+          <a href="#home">Home</a>
+          <a href="#settings">Settings</a>
+        </nav>
+        <form className="search-form" onSubmit={handleSearchSubmit}>
+          <input
+            type="text"
+            value={searchTerm}
+            onChange={handleSearchChange}
+            placeholder="Search..."
+            className="search-input"
+          />
+        </form>
+      </header>
+    </div>
+  );
+}
+
+export default Header;
+
